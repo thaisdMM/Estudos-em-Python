21ae989a8e4193853a4360aca0a614532cc15c2c
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-10 13:37:27 +0200

    Update 009_trabalhando_csv_lista_dicionario.py

--- manipulacao_arquivos/009_trabalhando_csv_lista_dicionario.py
+++ manipulacao_arquivos/009_trabalhando_csv_lista_dicionario.py
@@ -10,23 +10,31 @@ dados = [
 ]
 file_path = "dados_pessoas.csv"
 
-try:
-    with open(file_path, "w+") as write_file:
-        writer = csv.writer(write_file)
-        for row in dados:
-            writer.writerow(row)
-        print(f"File {file_path} was created!")
-except FileNotFoundError:
-    print("File csv was not found.")
-except PermissionError:
-    print("You don't have permission to created this file.")
+# try:
+#     with open(file_path, "w+") as write_file:
+#         writer = csv.writer(write_file)
+#         for row in dados:
+#             writer.writerow(row)
+#         print(f"File {file_path} was created!")
+# except FileNotFoundError:
+#     print("File csv was not found.")
+# except PermissionError:
+#     print("You don't have permission to created this file.")
 
-try:
-    with open(file_path, "r") as read_file:
-        content = csv.reader(read_file)
-        for line in content:
-            print(line)
-except FileNotFoundError:
-    print("File csv was not found.")
-except PermissionError:
-    print("You don't have permission to created this file.")
+# try:
+#     with open(file_path, "r") as read_file:
+#         content = csv.reader(read_file)
+#         for line in content:
+#             print(line)
+# except FileNotFoundError:
+#     print("File csv was not found.")
+# except PermissionError:
+#     print("You don't have permission to created this file.")
+
+with open(file_path, "r") as read_file:
+    content = csv.reader(read_file)
+    for line in content:
+        chaves = {(line)[0][:]}
+        print(line)
+        print(chaves)
+print(chaves)
