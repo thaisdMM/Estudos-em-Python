bfcf2c6c6b94c4136ac013fe9c8cc72b8317d2d8
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-26 11:34:42 +0200

    Create ex002_lamba_metade.py

---
+++ lambda/ex002_lamba_metade.py
@@ -0,0 +1,17 @@
+# Exercício 2 (fácil)
+# Use lambda e map() para transformar a lista [2, 4, 6, 8] em uma nova lista com os valores divididos por 2.
+
+# 1
+numeros = [2, 4, 6, 8]
+metade = list(map(lambda x: x / 2, numeros))
+print(numeros)
+print(metade)
+
+# 2 criando lista com input e fazendo lambda metada
+
+lista_numeros = []
+for i in range(0, 5):
+    lista_numeros.append(int(input("Digite 5 números inteiros para compor uma lista ")))
+print(lista_numeros)
+metadade = list(map(lambda x: x / 2, lista_numeros))
+print(metadade)
