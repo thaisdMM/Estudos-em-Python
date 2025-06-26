a1146d355cc88066eb50e15c44ffb78ca790b903
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-26 11:16:49 +0200

    Create ex001_lamba_dobro.py

---
+++ lambda/ex001_lamba_dobro.py
@@ -0,0 +1,8 @@
+# Exercício 1 (fácil)
+# Crie uma função lambda que receba um número e retorne o quadrado dele.
+
+dobro = lambda x: x * 2
+print(dobro(8))
+
+numero = float(input(f"Digite um número para ver o dobro dele "))
+print(dobro(numero))
