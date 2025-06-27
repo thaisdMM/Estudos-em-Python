b86a81ea0ce026e41066607fcba43ef148edfd80
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-27 14:00:34 +0200

    Update ex006_lambda_sorted_map_filter_alunos_notas_situacao.py

--- lambda/ex006_lambda_sorted_map_filter_alunos_notas_situacao.py
+++ lambda/ex006_lambda_sorted_map_filter_alunos_notas_situacao.py
@@ -28,23 +28,15 @@ alunos = [
     {"nome": "julia", "notas": [8, 7, 6]},
 ]
 
-media_alunos = list(map(lambda x: sum(x["notas"]) / len(x["notas"]), alunos))
-print(media_alunos)
-situacao_aluno = lambda x: (
-    "aprovado" if x >= 7 else "recuperação" if x >= 5 else "reprovado"
-)
+# media_alunos = list(map(lambda x: sum(x["notas"]) / len(x["notas"]), alunos))
+# print(media_alunos)
+# situacao_aluno = lambda x: (
+#     "aprovado" if x >= 7 else "recuperação" if x >= 5 else "reprovado"
+# )
 # for i in media_alunos:
 #     print(situacao_aluno(i))
 
-# alunos_media_situação = [alunos[:]]
-# for i in range(0, len(media_alunos)):
-#     # for valor in alunos_media_situação:
-#     #     valor["media"] = media_alunos[i]
-#         alunos_media_situação[i].append(valor["media"])
-#         # print(media_alunos)
-
 print(alunos)
-
 alunos_situacao = list(
     map(
         lambda x: {
@@ -71,83 +63,3 @@ print("\n Lista com ordenação por média descrecente:")
 ordem_alunos_media = sorted(alunos_situacao, key=lambda x: x["media"], reverse=True)
 for aluno in ordem_alunos_media:
     print(aluno)
-
-# alunos_situacao = list(
-#     map(
-#         lambda x: {"nome": x["nome"], "media": x["media"], "situação":
-#             "aprovado"
-#             if x["media"] >= 7
-#             else "recuperação" if x["media"] >= 5 else "reprovado"
-#         },
-#         alunos,
-#     )
-# )
-
-# # x["media"]>= 7 else "recuperação" if x['media']>=5 else "reprovado"}, alunos))
-
-# # alunos_situacao = list(map(lambda x: {"nome": x["nome"], "media": sum(x["notas"])/len(x["notas"]), "situação": "aprovado" if "media">= 7 else "recuperação" if 'media'>=5 else "reprovado"}, alunos))
-
-# print(alunos_situacao)
-
-
-# alunos_media_situação = []
-# for i in range(0, len(media_alunos)):
-#     for valor in alunos:
-#         print(valor)
-#         if valor["notas"]:
-#             del valor["notas"]
-#         else:
-#             continue
-#         valor['media'] = media_alunos[i]
-#     # print(media_alunos)
-
-# print(alunos)
-
-
-# alunos_media_situação = []
-# for i in range(len(alunos)+1):
-#     for valor in alunos:
-#         print(valor)
-#         valor["media"] = media_alunos[i]
-#         print(media_alunos)
-# valor['notas'] = valor['media']
-# alunos_media_situação.append(alunos[:])
-# print(valor)
-# print(alunos_media_situação)
-
-
-# for key, value in alunos.items():
-#     for media in media_alunos:
-#         alunos_media_situação.append()
-
-
-# for i in media_alunos:
-#     print(situacao_aluno(i))
-
-
-# for valor in alunos:
-#     for notas in valor['notas']:
-#         print(notas, end=" ")
-#         media_alunos = list(map(lambda x: sum(x) / len(x), notas))
-#         print(media_alunos)
-#     print()
-
-# for valor in alunos:
-#     print(valor['notas'])
-#     print(sum(valor['notas']))
-#     media = (sum(valor['notas'])/len(valor["notas"]))
-#     print(media)
-#     media_alunos = list(map(lambda valor["notas"]: (sum(valor['notas'])/len(valor["notas"]), valor["notas"]))
-#     print(media_alunos)
-#     # media2 = list(map(media, alunos))
-#     # print(f"map media {media2}")
-# #     notas = list(map(sum(valor["notas"]), alunos))
-# # print(notas)
-#     # media_alunos = list(map(lambda x: sum(x) / len(x), valor["notas"]))
-# print(media_alunos)
-# # print()
-
-# print(media_alunos)
-
-# media_alunos = list(map(lambda x: sum(x) / len(x), alunos[3]))
-# print(media_alunos)
