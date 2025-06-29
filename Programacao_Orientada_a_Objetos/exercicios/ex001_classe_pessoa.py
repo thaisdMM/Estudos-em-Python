2caac6229881f2ef7a1350f8e3766b353d44ac5f
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-29 19:44:13 +0200

    Create ex001_classe_pessoa.py

---
+++ Programacao_Orientada_a_Objetos/exercicios/ex001_classe_pessoa.py
@@ -0,0 +1,28 @@
+class Pessoa:
+    # metodo construtor
+    def __init__(self, nome, altura, idade):
+        # atributos:
+        self.nome = nome
+        self.altura = altura
+        self.idade = idade
+
+    # métodos
+    def correr(self, km):
+        print(f"Todos os dias eu corro {km}km.")
+
+    def comer(self, comida):
+        print(f"Eu amo comer: {comida}.")
+
+
+pessoa1 = Pessoa("João", 1.80, 35)
+print(f"Olá, meu nome é {pessoa1.nome}")
+print(f"Eu tenho {pessoa1.altura:.2f} de altura e {pessoa1.idade} anos de idade.")
+pessoa1.comer("Hambúrguer com batata frita e coca-cola.")
+pessoa1.correr(10)
+
+
+pessoa2 = Pessoa("Ana", 1.55, 28)
+print(f"Olá, meu nome é {pessoa2.nome}")
+print(f"Eu tenho {pessoa2.altura:.2f} de altura e {pessoa2.idade} anos de idade.")
+pessoa2.comer("comidas saudáveis: salada é minha comida favorita")
+pessoa2.correr(5)
