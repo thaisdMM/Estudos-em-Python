beb5dedbdf35d52cf3f7dc93ee2c1cad1b0d7955
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-07-04 19:13:12 +0200

    learning Acessing and Modifying Data: protected vs private and get and set methods

--- Programacao_Orientada_a_Objetos/aulas/aula004_freecode_acessing_modifying_object_data.py
+++ Programacao_Orientada_a_Objetos/aulas/aula004_freecode_acessing_modifying_object_data.py
@@ -44,16 +44,109 @@
 # 1- The traditional way: make the data private and use getters and setters:
 
 
+# class User:
+#     def __init__(self, username, email, passoword):
+#         self.username = username
+#         # atributo protegido _ desses antes do nome ex: _email
+#         self._email = email
+#         self.passoword = passoword
+
+# def get_email(self):
+#     return self._email
+
+
+# user1 = User("robynwood", "robyn@gmail.com", "123")
+# # não pode acessar um atributo protegido fora da classe:
+# # em python você consegue acessar o método protegido, fora da classe, mas é errado ainda assim.
+# # > o código abaixo está errado:
+# # print(user1._email)
+
+
+# class User:
+#     def __init__(self, username, email, passoword):
+#         self.username = username
+#         # atributo protegido _ desses antes do nome ex: _email
+#         self._email = email
+#         self.passoword = passoword
+
+#     # utilizando um método para usar o atributo protegido
+#     def clean_email(self):
+#         return self._email.lower().strip()
+
+
+# user1 = User("robynwood", "  Robyn@gmail.com  ", "123")
+
+# print(user1.clean_email())
+
+## PRIVATE != PROTECTED
+## Name Mangled(Python "troca o nome do atributo")
+
+
+# class User:
+#     def __init__(self, username, email, passoword):
+#         self.username = username
+#         # atributo PRIVADO __ 2 desses antes do nome ex: __email
+#         self.__email = email
+#         self.passoword = passoword
+
+#     # utilizando um método para usar o atributo protegido
+#     def clean_email(self):
+#         return self.__email.lower().strip()
+
+
+# user1 = User("robynwood", "  Robyn@gmail.com  ", "123")
+
+# # #impossível acessar fora da classe quando é privado - Python "troca o nome do atributo"
+# # print(user1.__email)
+# # output an ERROR: AttributeError: 'User' object has no attribute '__email'
+
+# print(user1.clean_email())
+
+# ## when to use protected vs private attributes?
+# # normalmente usa o protegido.
+# # usa o privado quando é extremamente necessário.
+
+## Creating getter and setter methods
+
+## Why do we create getters and setters?
+from datetime import datetime
+
+
 class User:
     def __init__(self, username, email, passoword):
         self.username = username
-        # atributo protegido _ desses antes do nome ex: _email
+        # atributo PROTEGIDO
         self._email = email
         self.passoword = passoword
 
+    # esse jeito é convencional
     def get_email(self):
+        ## Why do we create getters and setters?
+        ## pode controlar se a pessoa tem autorização para acessar esse email
+        ## controla a hora que o email foi acessado
+        print(f"Email acessed at {datetime.now()}")
         return self._email
+
+    # def set_email(self, new_email):
+    #     self._email = new_email
+    # colocando um pouco de validação lógica no set método:
+    def set_email(self, new_email):
+        if "@" in new_email:
+            self._email = new_email
 
 
 user1 = User("robynwood", "robyn@gmail.com", "123")
-print(user1.email)
+
+print(user1.get_email())
+user1.set_email("rob@outlook.com")
+print(user1.get_email())
+
+# para evitar esse tipo de dado vamos adicionar uma pequena lógica no set método
+user1.set_email("2345gfg")
+print(user1.get_email())  # output: 2345gfg
+# rodando o print depois da validação com o if:
+# caso 1 o email não foi atualizado outup: Email acessed at 2025-07-04 19:01:17.153200 rob@outlook.com
+
+user1.set_email("2345@gfg.com")
+print(user1.get_email())
+# aqui é atualizado: Email acessed at 2025-07-04 19:06:08.418298 2345@gfg.com
